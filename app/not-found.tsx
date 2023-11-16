6044dc80d359e5f27250eac20ca2b766cc7de52a
최유리 <choiyr5256@gmail.com>
2023-11-16 10:30:14 +0900

    server 수정

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default async function NotFound(){
     // console.log(headerList.get('referer'))
     return(
         <>
-            <p>입력하신 {domain}은 없는 페이지 입니다.</p>
+            <p>입력하신 {domain}은 없는 페이지 입니다</p>
         </>
     )
 }
